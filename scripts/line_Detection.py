07e2fdeab2de10b22cc3c13c649845c858bdce77
Dominic Nightingale <djnighti@ucsd.edu>
2021-04-24 16:51:21 +0000

    Update line_Detection.py

--- scripts/line_Detection.py
+++ scripts/line_Detection.py
@@ -2,7 +2,7 @@
 import rospy
 import cv2
 import numpy as np
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Int32MultiArray
 from sensor_msgs.msg import Image
 from decoder import decodeImage
 
@@ -46,34 +46,37 @@ def video_detection(data):
     mask = cv2.inRange(hsv, lower, upper)
 
     m = cv2.moments(mask, False)
+    centroid_and_frame_width = []
     try:
         cx, cy = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
     except ZeroDivisionError:
         cy, cx = int(height / 2), int(width / 2)
 
-    # Publish centroid
+    # Publish Message
     mid_x.data = cx
     mid_y.data = cy
     cv2.circle(img, (mid_x.data, mid_y.data), 7, (255, 0, 0), -1)
-    pub.publish(mid_x)
+    centroid_and_frame_width.append(mid_x)
+    centroid_and_frame_width.append(width)
+    pub.publish(centroid_and_frame_width)
 
     # plotting results
-    try:
-        cv2.imshow("original", orig)
-        cv2.imshow("yellow mask", mask)
-        cv2.imshow("plotting centroid", img)
-        cv2.waitKey(1)
-    except KeyboardInterrupt:
-        cv2.destroyAllWindows()
-
-
-def main():
-    rospy.init_node('lane_detection_node', anonymous=True)
-    camera_sub = rospy.Subscriber('camera_rgb', Image, video_detection)
-    rate = rospy.Rate(5)
-    rospy.spin()
-
+    # try:
+    #     cv2.imshow("original", orig)
+    #     cv2.imshow("yellow mask", mask)
+    #     cv2.imshow("plotting centroid", img)
+    #     cv2.waitKey(1)
+    # except KeyboardInterrupt:
+    #     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    main()
+    rospy.init_node('lane_detection_node', anonymous=False)
+    camera_sub = rospy.Subscriber('camera_rgb', Image, video_detection)
+    pub = rospy.Publisher('/centroid', Int32MultiArray, queue_size=2)
+    rate = rospy.Rate(15)
+    while not rospy.is_shutdown():
+        rospy.spin()
+        rate.sleep()
+
+
