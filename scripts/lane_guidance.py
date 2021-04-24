9ca48e38a32557d3582933ba63376025f7d5d818
Dominic Nightingale <djnighti@ucsd.edu>
2021-04-24 16:38:44 +0000

    Update lane_guidance.py

--- scripts/lane_guidance.py
+++ scripts/lane_guidance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float32, Int32
+from std_msgs.msg import Float32, Int32, Int32MultiArray
 
 global steering_float, throttle_float
 steering_float = Float32()
@@ -12,14 +12,15 @@ def LineFollower(msg):
     global steering_float, throttle_float
     steering_float = Float32()
     throttle_float = Float32()
-    width = 800  # width of camera frame
+    centroid = msg[0]
+    width = msg[1]  # width of camera frame
 
     if msg.data == 0:
         error_x = 0
-        throttle_float = 0.135
+        throttle_float = 0.95
     else:
-        error_x = float(((msg.data) - width / 2))
-        throttle_float = 0.14
+        error_x = float(centroid - (width / 2))
+        throttle_float = 1.0
 
     steering_float = float(kp * (error_x / (width / 2)))
     if steering_float < -1:
@@ -28,19 +29,16 @@ def LineFollower(msg):
         steering_float = 1
     else:
         pass
-
-
-def main():
-    rospy.init_node('lane_guidance_node', anonymous=True)
-    centroid_subscriber = rospy.Subscriber('/centroid', Int32, LineFollower)
-    steering_pub = rospy.Publisher('steering', Float32, queue_size=1)
-    throttle_pub = rospy.Publisher('throttle', Float32, queue_size=1)
-    rate = rospy.Rate(30)
-    while not rospy.is_shutdown():
-        steering_pub.publish(steering_float)
-        throttle_pub.publish(throttle_float)
-        rate.sleep()
+    steering_pub.publish(steering_float)
+    throttle_pub.publish(throttle_float)
 
 
 if __name__ == '__main__':
-    main()
+    rospy.init_node('lane_guidance_node', anonymous=True)
+    centroid_subscriber = rospy.Subscriber('/centroid', Int32MultiArray, LineFollower)
+    steering_pub = rospy.Publisher('steering', Float32, queue_size=1)
+    throttle_pub = rospy.Publisher('throttle', Float32, queue_size=1)
+    rate = rospy.Rate(15)
+    while not rospy.is_shutdown():
+        rospy.spin()
+        rate.sleep()
