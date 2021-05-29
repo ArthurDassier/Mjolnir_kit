d1ebba01a92f484af36f1aa26682b453e763f5c9
Arthur Dassier <dassierarthur@gmail.com>
2021-05-29 10:27:27 -0700

    [MODIF] Class ESP32 function poll renamed get_input

--- scripts/class_ESP32.py
+++ scripts/class_ESP32.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import serial
 import re
 
@@ -9,25 +10,16 @@ class ESP32_Mjolnir():
         self.steering = 0.0
         self.speed = 0.0
 
-    def poll(self):
-        """Get input values from ESP32 in manual mode"""
-        # Go over all messages (one per line) in the serial buffer
-        # Store the values on board (speed,throttle,steering)
-        # Check next message
-        # If none left, poll function is done
+    def get_input(self):
+        """Get input values from ESP32"""
         while self.ser.in_waiting:
             mcu_message = self.ser.readline().decode().lower()  # The message coming in
             number_in_message = re.findall(r'\d+\.?\d*', mcu_message)  # Find number in message
 
-            print("message from ESP32 :" + str(mcu_message))
+            print("message from ESP32 : " + str(mcu_message))
 
-            # self.watchdog_subthread.reset_countdown()  # Reset watchdog as soon as data is received
-            if 'speed' in mcu_message:
-                self.speed = number_in_message[0]
-            elif 'throttle' in mcu_message:
-                self.throttle = number_in_message[0]
-            elif 'steering' in mcu_message:
-                self.steering = number_in_message[0]
+            # if 'responseSpeed_' in mcu_message:
+            #     self.speed = number_in_message[0]
 
     def emergency_stop(self):
         msg = f'STOP\n'
